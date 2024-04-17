4fd8965807b4502b9e046592e5889181e341ebda
Favour A. Uzochukwu <uzochukwu.favour310@gmail.com>
2024-04-17 16:35:46 +0100

    Importing List from typing in app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -5,6 +5,7 @@ import os
 from os import getenv
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
+from typing import List
 
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
